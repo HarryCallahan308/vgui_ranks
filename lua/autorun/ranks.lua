436116a7a71ea88b1cd859c70f7af327a8f3ed65
Harry Callahan <jringel2@gmail.com>
2020-08-14 11:59:07 -0400

    Add clientside code and create dframe

---
+++ lua/autorun/ranks.lua
@@ -0,0 +1,33 @@
+if not CLIENT then return end
+
+PANEL = {}
+
+local sw, sh = ScrW(), ScrH
+
+-- Add materials
+function PANEL:Init()
+
+	self:SetSize( sw * .7, sh * .6)
+	self:Center()
+	self:SetSizable( true )
+	self:SetFont( "Roboto" )
+	self:SetTitle( "CFC Ranks" )
+
+end
+
+function PANEL:Paint(w, h)
+
+	draw.RoundedBox( 0, 0, 0, w, h, color_black )
+
+end
+
+function PANEL:Add()
+
+	self:Add( "CFC_Rank_Custom_DCollapsibleCategory" )
+
+end
+
+vgui.Register( "CFC_Rank_Custom_DFrame", PANEL, "DFrame" )
+
+
+
